26187036427fc511eb42f84871762b1c51d18dae
MongoTarzan <tizian.k@gmx.at>
2022-09-16 14:13:33 +0200

    inital commit

---
+++ test01/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test01
+{
+     class Gebaeude
+    {
+        string Name;
+        public string NameAccessible
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+    }
+}
